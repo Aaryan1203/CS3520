9ad381ab8f25780f65b17f8f72a086f08355918c
aaryan1203 <aaryanjain1203@gmail.com>
2024-05-06 17:27:01 -0400

    pa2

---
+++ Practice Activities/pa2/hello/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "    A     aaaaa   rrrrr   y   y  aaaaa  n   n\n";
+    cout << "   A A    a   a   r   r    y y   a   a  nn  n\n";
+    cout << "  AAAAA   aaaaa   rrrr      y    aaaaa  n n n\n";
+    cout << " A     A  a   a   r  r      y    a   a  n  nn\n";
+    cout << "A       A a   a   r   r     y    a   a  n   n\n";
+    return 0;
+}
