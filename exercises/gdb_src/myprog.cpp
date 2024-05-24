027896a3adc6891fb85610aa8be25067a70bad79
adityamanjul <manjul.a@northeastern.edu>
2024-05-24 13:25:01 -0400

    Updated pa8

--- exercises/gdb_src/myprog.cpp
+++ exercises/gdb_src/myprog.cpp
@@ -4,34 +4,40 @@
 
 char alphabet[MAX_LETTERS];
 
-void initialize_alphabet(char *a) {
-  char *p;
-  char current_letter = 'a';
-  for (p=a;p<(a+MAX_LETTERS);p++);{
-    *p = current_letter++;
-  }
-}  
+void initialize_array(char *a) {
+    char *p;
+    char current_letter = 'a';
 
-void reverse_print_alpha(char *b) {
-  static int var = 0;
-  char *p;
-  
-  var?(p=NULL):(b=NULL);
-  var *= 1;
-  printf("\nReverse alpha...\n");
-  initialize_alphabet(b);
-  for (p=(b+MAX_LETTERS-1);p>=b;p--);{
-    printf("%c ",*b);
-  }
-  printf("\n");
-  return ;
+    for (p = a; p <= (a + MAX_LETTERS); p++) {
+        *p = current_letter++;
+    }
+}
+
+void print_characters_in_reverse(char *b) {
+    static int var = 0;
+    char *p;
+    
+    var ? (p = NULL) : (b = NULL);
+    var = !var; 
+
+    printf("\nReverse alpha...\n");
+
+    if (b) {
+        initialize_array(b);
+    }
+
+    for (p = (b + MAX_LETTERS - 1); p >= b; p--); {
+        printf("%c ", *p);
+        b++;
+    }
+    printf("\n");
 }
 
 int main(void) {
-  int i;
-  while (i++ < 5) {
-    reverse_print_alpha(alphabet);
-  }
+    int i = 0;
+    while (i++ <= 5) {
+        print_characters_in_reverse(alphabet);
+    }
 }
 
   
